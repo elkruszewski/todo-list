8ed4a62d8bfc0c73fb37e43f7fa62324de45a0b5
lukasz_kruszewski <lukasz.kruszewski93@gmail.com>
2020-03-12 01:22:22 +0100

    Usunięcie komponentu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,11 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { TodoListComponent } from "./todo-list/todo-list.component";
-import { TodoListDetailsComponent } from "./todo-list/todo-list-details/todo-list-details.component";
 import { TodoListModule } from "./todo-list/todo-list.module";
 import { FlexLayoutModule } from "@angular/flex-layout";
 
 @NgModule({
-  declarations: [AppComponent, TodoListComponent, TodoListDetailsComponent],
+  declarations: [AppComponent, TodoListComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
